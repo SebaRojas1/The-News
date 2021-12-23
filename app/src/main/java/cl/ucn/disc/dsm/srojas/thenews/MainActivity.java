efe3dfac4672bf5cfa3c05de99119ffdfd2b00e2
SebaRojas1 <seba.rojas002@gmail.com>
2021-12-22 21:13:39 -0300

    Feat: adding the recycler view on the proyect

--- app/src/main/java/cl/ucn/disc/dsm/srojas/thenews/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/srojas/thenews/MainActivity.java
@@ -3,12 +3,34 @@ package cl.ucn.disc.dsm.srojas.thenews;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import cl.ucn.disc.dsm.srojas.thenews.model.NewsAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> listNews;
+    RecyclerView recycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recycler = (RecyclerView) findViewById(R.id.RecyclerId);
+        recycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL
+                , false));
+
+        listNews = new ArrayList<String>();
+
+        for (int i=0; i<50; i++) {
+            listNews.add("Dato "+ i +"");
+        }
+
+        NewsAdapter adapter = new NewsAdapter(listNews);
+        recycler.setAdapter(adapter);
     }
 }
